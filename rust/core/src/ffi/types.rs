3fdf3790bd0e0d0c2ed9f5a65266ce3d2c429811
Felipe Oliveira Carvalho <felipekde@gmail.com>
2025-05-14 17:44:07 -0300

    fix(rust/core): Ensure a signal stack is allocated when calling into Go code (#16)

--- rust/core/src/ffi/types.rs
+++ rust/core/src/ffi/types.rs
@@ -22,6 +22,7 @@ use std::mem::ManuallyDrop;
 use std::os::raw::{c_char, c_int, c_void};
 use std::ptr::{null, null_mut};
 
+use super::signal::SignalStackGuard;
 use super::{constants, methods};
 use crate::{
     error::{Error, Status},
@@ -636,6 +637,7 @@ unsafe extern "C" fn release_ffi_error(error: *mut FFI_AdbcError) {
 impl Drop for FFI_AdbcError {
     fn drop(&mut self) {
         if let Some(release) = self.release {
+            let _ = SignalStackGuard::new();
             unsafe { release(self) };
         }
     }
@@ -644,6 +646,7 @@ impl Drop for FFI_AdbcError {
 impl Drop for FFI_AdbcDriver {
     fn drop(&mut self) {
         if let Some(release) = self.release {
+            let _ = SignalStackGuard::new();
             // TODO(alexandreyc): how should we handle `release` failing?
             // See: https://github.com/apache/arrow-adbc/pull/1742#discussion_r1574388409
             unsafe { release(self, null_mut()) };
@@ -654,6 +657,7 @@ impl Drop for FFI_AdbcDriver {
 impl Drop for FFI_AdbcPartitions {
     fn drop(&mut self) {
         if let Some(release) = self.release {
+            let _ = SignalStackGuard::new();
             unsafe { release(self) };
         }
     }
